5801eda724df859ab93ea7e9bebf6059a95aa5cc
Edison Garcia Chinas <edisga@microsoft.com>
2020-05-19 07:54:45 -0500

    Updating scripts directory

---
+++ scripts/postinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Running postinstall script - Listing all content inside temp...."
+ls temp
+echo "Running postinstall script - Deleting temp folder...."
+rm -rf temp
---
+++ scripts/preinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Running preinstall script...."
+mkdir temp
+echo "Running preinstall script - Creating temp folder...."
+cd temp
+echo "Running preinstall script - Creating file inside temp folder...."
+touch test.txt
