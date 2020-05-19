42250bdd8588d1d02e271f5a12aa5d2968d15869
Edison Garcia Chinas <edisga@microsoft.com>
2020-05-19 08:16:28 -0500

    Adding package.json scripts

---
+++ scripts/package-postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Running postinstall script from package.json ...."
+pwd
+ls -ltra
---
+++ scripts/package-preinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Running preinstall script from package.json ...."
+pwd
+ls temp
